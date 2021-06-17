f0398c1312f08f079af6a41d57f7d9ad3d43fbe8
Iris <kern.iris@posteo.de>
2021-06-17 15:11:44 +0200

    Add events to homepage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import EventCard from "../components/EventCard";
 import styled from "styled-components/macro";
 
 export default function Homepage() {
-  return <Wrapper>hallo</Wrapper>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("events")
+      .then((response) => response.data)
+      .then(setEvents)
+      .catch((error) => console.log(error.message));
+  }, []);
+
+  return (
+    <Wrapper>
+      <h1>Event Highlights</h1>
+
+      <div>
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    background: #effffa;
+    font-size: 100%;
+    text-align: center;
+    padding: 40px 15px;
+  }
+`;
