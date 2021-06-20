24404fec86b3e6f9f6a7b765d48328e15b7a15d5
Iris <kern.iris@posteo.de>
2021-06-20 23:22:18 +0200

    Init event details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import Layout from "./globalLayout/Layout";
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/home" exact>
-          <Homepage />
-        </Route>
-      </Switch>
-    </Router>
+    <Layout>
+      <Router>
+        <Switch>
+          <Route path="/home" exact>
+            <Homepage />
+          </Route>
+          <Route path="event/:id/details" exact>
+            <EventDetailsPage />
+          </Route>
+        </Switch>
+      </Router>
+    </Layout>
   );
 }
