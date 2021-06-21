aa722dd7e089a5ca895f48e88a3dce1d6962ec32
Iris <kern.iris@posteo.de>
2021-06-21 19:03:41 +0200

    Edit app js routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layout from "./globalLayout/Layout";
 import Homepage from "./pages/Homepage";
 import EventDetailsPage from "./pages/EventDetailsPage";
-import Layout from "./globalLayout/Layout";
 
 export default function App() {
   return (
     <Layout>
       <Router>
         <Switch>
-          <Route component={Homepage} path="/home" exact />
-          <Route
-            component={EventDetailsPage}
-            path="/events/:id/details"
-            exact
-          />
+          <Route path="/home" exact>
+            <Homepage />
+          </Route>
+          <Route path="/events/:id/details" exact>
+            <EventDetailsPage />
+          </Route>
         </Switch>
       </Router>
     </Layout>
