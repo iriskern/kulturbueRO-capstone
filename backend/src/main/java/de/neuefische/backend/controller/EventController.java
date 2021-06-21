ac200cfd3cba0df2131e349b39ba7e5719b100e6
Iris <kern.iris@posteo.de>
2021-06-21 10:58:11 +0200

    Add event details route in backend

--- backend/src/main/java/de/neuefische/backend/controller/EventController.java
+++ backend/src/main/java/de/neuefische/backend/controller/EventController.java
@@ -3,11 +3,15 @@ package de.neuefische.backend.controller;
 import de.neuefische.backend.model.Event;
 import de.neuefische.backend.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("events")
@@ -23,5 +27,16 @@ public class EventController {
     @GetMapping
     public List<Event> listAllEvents() {
         return eventService.listAllEvents();
+    }
+
+    @GetMapping("/{id}/details")
+    public Event getEventById(@PathVariable String id) {
+        Optional<Event> optionalEvent = eventService.getEventById(id);
+
+        if(optionalEvent.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "event does not exist");
+        }
+
+        return optionalEvent.get();
     }
 }
