89e1c78e42d59e88839da2bb9e504b844937e5f1
Iris <kern.iris@posteo.de>
2021-06-23 10:37:37 +0200

    Add event calendar page

---
+++ frontend/src/pages/EventCalendarPage.js
@@ -0,0 +1,49 @@
+import useEvents from "../hooks/useEvents";
+import EventCard from "../components/EventCard";
+import styled from "styled-components/macro";
+import { LocalDateTime } from "@js-joda/core";
+
+export default function EventCalendarPage() {
+  const { events } = useEvents();
+
+  return (
+    <Wrapper>
+      <h1>Event Calendar</h1>
+
+      <div>
+        {events
+          .sort((a, b) =>
+            LocalDateTime.parse(a.dateTime).isBefore(
+              LocalDateTime.parse(b.dateTime)
+            )
+              ? -1
+              : LocalDateTime.parse(a.dateTime).isAfter(
+                  LocalDateTime.parse(b.dateTime)
+                )
+              ? 1
+              : 0
+          )
+          .map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    background: #effffa;
+    font-size: 100%;
+    text-align: center;
+    padding: 20px 15px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
