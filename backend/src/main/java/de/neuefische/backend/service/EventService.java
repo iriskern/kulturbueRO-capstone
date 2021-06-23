7072715d94c3010d0f8429902556cc5ff90cc989
Iris <kern.iris@posteo.de>
2021-06-23 15:20:00 +0200

    Add dbref annotation to event model

--- backend/src/main/java/de/neuefische/backend/service/EventService.java
+++ backend/src/main/java/de/neuefische/backend/service/EventService.java
@@ -1,6 +1,7 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.model.Event;
+import de.neuefische.backend.model.Location;
 import de.neuefische.backend.repos.EventRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,12 @@ import java.util.Optional;
 public class EventService {
 
     private final EventRepo eventRepo;
+    private final LocationService locationService;
 
     @Autowired
-    public EventService(EventRepo eventRepo) {
+    public EventService(EventRepo eventRepo, LocationService locationService) {
         this.eventRepo = eventRepo;
+        this.locationService = locationService;
     }
 
     public List<Event> listAllEvents() {
@@ -24,5 +27,23 @@ public class EventService {
 
     public Optional<Event> getEventById(String id) {
         return eventRepo.findById(id);
+    }
+
+    public Event createEvent(Event event, Location location) {
+        Event newEvent = Event.builder()
+                .title(event.getTitle())
+                .description(event.getDescription())
+                .dateTime(event.getDateTime())
+                .location(locationService.getLocationById(location.getId()).get())
+                .eventSetting(event.getEventSetting())
+                .eventTypes(event.getEventTypes())
+                .pictureUrl(event.getPictureUrl())
+                .infoUrl(event.getInfoUrl())
+                .ticketUrl(event.getTicketUrl())
+                .entranceFee(event.getEntranceFee())
+                .highlightEvent(event.isHighlightEvent())
+                .build();
+
+        return eventRepo.save(newEvent);
     }
 }
