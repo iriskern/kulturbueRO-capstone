6c7a25ffe5be89b83df43ec66b4590b1046cd9f1
Iris <kern.iris@posteo.de>
2021-06-28 08:37:44 +0200

    Add map page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import LocationsPage from "./pages/LocationsPage";
 import EventDetailsPage from "./pages/EventDetailsPage";
 import EventCalendarPage from "./pages/EventCalendarPage";
+import MapPage from "./pages/MapPage";
 
 export default function App() {
   return (
@@ -20,6 +21,12 @@ export default function App() {
         </Route>
         <Route path="/locations">
           <LocationsPage />
+        </Route>
+        <Route path="/map">
+          <MapPage />
+        </Route>
+        <Route path="/map/:id">
+          <MapPage />
         </Route>
       </Switch>
     </Layout>
