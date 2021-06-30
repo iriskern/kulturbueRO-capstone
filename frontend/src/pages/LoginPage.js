19e925c2e0e718b0e42b29e7e0e36639f366fd96
Iris <kern.iris@posteo.de>
2021-06-30 19:13:12 +0200

    Styled login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,13 @@
 import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
+import styled from "styled-components/macro";
+
+const initialState = {
+  username: "",
+  password: "",
+};
 
 export default function LoginPage() {
-  const initialState = {
-    username: "",
-    password: "",
-  };
-
   const [credentials, setCredentials] = useState(initialState);
   const { login } = useContext(AuthContext);
 
@@ -20,28 +21,76 @@ export default function LoginPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username
-        <input
-          placeholder="enter username"
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={credentials.username}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          placeholder="enter password"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={credentials.password}
-        />
-      </label>
-      <button>Login</button>
-    </form>
+    <Wrapper>
+      <h2>LOGIN</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={credentials.username}
+          />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={credentials.password}
+          />
+        </label>
+        <button disabled={!credentials.password || !credentials.username}>
+          Login
+        </button>
+      </form>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  background: #effffa;
+  width: 330px;
+  padding: 0 0 20px;
+  margin: 30px auto 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0 40px rgba(0, 0, 0, 0.08);
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    margin: 35px auto 5px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+  }
+
+  p {
+    margin: 25px 0 2px 0;
+  }
+
+  input {
+    font-size: 80%;
+    padding-top: 4px;
+    width: 300px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+  }
+
+  button {
+    background: #ecf765;
+    text-align: center;
+    margin: 35px 0 5px;
+  }
+
+  button:disabled {
+    background-color: lightgray;
+    color: darkgray;
+  }
+`;
