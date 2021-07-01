56f83b80036dba5f296c36a9ada87ba2e1ce6e15
Iris <kern.iris@posteo.de>
2021-07-01 10:34:42 +0200

    Fix event controller test

--- backend/src/test/java/de/neuefische/backend/controller/EventControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/EventControllerTest.java
@@ -1,31 +1,41 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.dto.LoginData;
 import de.neuefische.backend.model.Event;
 import de.neuefische.backend.model.EventSetting;
-import de.neuefische.backend.model.EventType;
 import de.neuefische.backend.repo.EventRepo;
+import de.neuefische.backend.security.model.AppUser;
+import de.neuefische.backend.security.repo.AppUserRepo;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "jwt.secret=some-jwt-secret")
 class EventControllerTest {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private AppUserRepo appUserRepo;
+
+    @Autowired
+    private PasswordEncoder encoder;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -38,8 +48,20 @@ class EventControllerTest {
         eventRepo.deleteAll();
     }
 
+    private HttpHeaders login() {
+        appUserRepo.save(AppUser.builder()
+                .username("Bob")
+                .password(encoder.encode("1234"))
+                .build());
+        LoginData loginData = new LoginData("Bob", "1234");
+
+        ResponseEntity<String> response = testRestTemplate.postForEntity("http://localhost:"+port+"/auth/login", loginData, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(Objects.requireNonNull(response.getBody()));
+        return headers;
+    }
+
     @Test
-    @DisplayName("List all events")
     public void listAllEventsTest() {
         //Given
         eventRepo.saveAll(List.of(
@@ -49,9 +71,7 @@ class EventControllerTest {
                         .description("description1")
                         .dateTime(LocalDateTime.of(2021, 1,1,10,0))
                         .eventSetting(EventSetting.OUTDOOR)
-                        .eventTypes(List.of(
-                                EventType.MUSIC.description
-                        ))
+                        .eventTypes(List.of("Musik"))
                         .pictureUrl("picture1")
                         .infoUrl("info1")
                         .ticketUrl("ticket1")
@@ -64,9 +84,7 @@ class EventControllerTest {
                         .description("description2")
                         .dateTime(LocalDateTime.of(2021,2,2,20,0))
                         .eventSetting(EventSetting.OUTDOOR)
-                        .eventTypes(List.of(
-                                EventType.MUSIC.description
-                        ))
+                        .eventTypes(List.of("Musik"))
                         .pictureUrl("picture2")
                         .infoUrl("info2")
                         .ticketUrl("ticket2")
@@ -76,7 +94,8 @@ class EventControllerTest {
         ));
 
         //When
-        ResponseEntity<Event[]> response = testRestTemplate.getForEntity("http://localhost:" + port + "/events", Event[].class);
+        HttpHeaders headers = login();
+        ResponseEntity<Event[]> response = testRestTemplate.exchange("http://localhost:"+port+"/events", HttpMethod.GET, new HttpEntity<>(headers), Event[].class);
 
         //Then
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
@@ -87,9 +106,7 @@ class EventControllerTest {
                         .description("description1")
                         .dateTime(LocalDateTime.of(2021, 1,1,10,0))
                         .eventSetting(EventSetting.OUTDOOR)
-                        .eventTypes(List.of(
-                                EventType.MUSIC.description
-                        ))
+                        .eventTypes(List.of("Musik"))
                         .pictureUrl("picture1")
                         .infoUrl("info1")
                         .ticketUrl("ticket1")
@@ -102,9 +119,7 @@ class EventControllerTest {
                         .description("description2")
                         .dateTime(LocalDateTime.of(2021,2,2,20,0))
                         .eventSetting(EventSetting.OUTDOOR)
-                        .eventTypes(List.of(
-                                EventType.MUSIC.description
-                        ))
+                        .eventTypes(List.of("Musik"))
                         .pictureUrl("picture2")
                         .infoUrl("info2")
                         .ticketUrl("ticket2")
