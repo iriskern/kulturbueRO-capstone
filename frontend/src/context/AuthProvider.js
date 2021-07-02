778aca8b1dc683018d9da6251303cac520887094
Iris <kern.iris@posteo.de>
2021-07-02 12:57:18 +0200

    Add decoded user data to auth provider

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -2,17 +2,22 @@ import AuthContext from "./AuthContext";
 import { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 export default function AuthProvider({ children }) {
   const history = useHistory();
   const [token, setToken] = useState();
+  const [userData, setUserData] = useState();
   const [invalidLogin, setInvalidLogin] = useState(false);
 
   const login = (credentials) => {
     axios
       .post("/auth/login", credentials)
       .then((response) => response.data)
-      .then(setToken)
+      .then((data) => {
+        setToken(data);
+        setUserData(jwt_decode(data.toString()));
+      })
       .then(() => history.push("/home"))
       .catch((error) => {
         setInvalidLogin(true);
@@ -21,7 +26,7 @@ export default function AuthProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, invalidLogin }}>
+    <AuthContext.Provider value={{ login, token, userData, invalidLogin }}>
       {children}
     </AuthContext.Provider>
   );
