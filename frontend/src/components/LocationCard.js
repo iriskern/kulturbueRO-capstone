894a297960cbf8a1424c25b2f103571c25808b4b
Iris <kern.iris@posteo.de>
2021-07-04 12:42:59 +0200

    Add like button to location card

--- frontend/src/components/LocationCard.js
+++ frontend/src/components/LocationCard.js
@@ -1,13 +1,40 @@
 import styled from "styled-components/macro";
 import { useHistory } from "react-router-dom";
+import { useContext, useState } from "react";
+import useWatchedLocations from "../hooks/useWatchedLocations";
+import AuthContext from "../context/AuthContext";
+import Heart from "react-heart";
 
 export default function LocationCard({ location }) {
   const history = useHistory();
   const handleClick = () => history.push(`/locations/map/${location.id}`);
 
+  const { userData } = useContext(AuthContext);
+  const { updateLocationInWatchlist } = useWatchedLocations();
+  const handleLike = (evt) => {
+    evt.stopPropagation();
+    updateLocationInWatchlist(location);
+  };
+  const [active, setActive] = useState(
+    location.watchedBy.includes(userData.sub)
+  );
+
   return (
     <CardWrapper onClick={handleClick}>
-      <img src={location.pictureUrl} alt={""} />
+      <div>
+        {userData && (
+          <LikeButton onClick={handleLike}>
+            <Heart
+              isActive={active}
+              onClick={() => setActive(!active)}
+              inactiveColor="#ecf765"
+              activeColor="#ecf765"
+            />
+          </LikeButton>
+        )}
+        <img src={location.pictureUrl} alt={""} />
+      </div>
+
       <h2>{location.name}</h2>
       <a
         onClick={(event) => event.stopPropagation()}
@@ -42,8 +69,18 @@ const CardWrapper = styled.section`
   overflow: hidden;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
+  position: relative;
 
   h3 {
     font-size: 80%;
   }
+`;
+
+const LikeButton = styled.button`
+  width: 55px;
+  position: absolute;
+  top: 0;
+  right: 5px;
+  background-color: transparent;
+  margin: 0;
 `;
