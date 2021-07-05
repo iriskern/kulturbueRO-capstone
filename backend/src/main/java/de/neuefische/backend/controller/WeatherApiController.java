b001e4d789e9d8e8f319a0693924a0b2f392b6ac
Iris <kern.iris@posteo.de>
2021-07-05 14:18:09 +0200

    Init weather api controller

---
+++ backend/src/main/java/de/neuefische/backend/controller/WeatherApiController.java
@@ -0,0 +1,30 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.dto.WeatherApiRequestDto;
+import de.neuefische.backend.model.weatherapi.WeatherApiResponse;
+import de.neuefische.backend.service.WeatherApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherApiController {
+
+    private final WeatherApiService weatherApiService;
+
+    @Autowired
+    public WeatherApiController(WeatherApiService weatherApiService) {
+        this.weatherApiService = weatherApiService;
+    }
+
+    @GetMapping
+    public List<WeatherApiResponse> getWeatherData(@RequestBody WeatherApiRequestDto coordinates) {
+        return weatherApiService.getWeatherData(coordinates);
+    }
+
+}
