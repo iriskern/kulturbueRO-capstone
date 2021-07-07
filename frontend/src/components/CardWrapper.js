1d81e6628ec8d0b58f8c4d0100b28df35ebe6375
Iris <kern.iris@posteo.de>
2021-07-07 16:27:15 +0200

    Init card wrapper component

---
+++ frontend/src/components/CardWrapper.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+export default styled.section`
+  background: #effffa;
+  width: 330px;
+  padding: 0 0 20px;
+  margin: 25px auto 10px;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0 40px rgba(0, 0, 0, 0.08);
+  border-radius: 5px;
+  position: relative;
+`;
